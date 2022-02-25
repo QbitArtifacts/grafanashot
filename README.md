d7a6a9d54535225ac257dd368494b507a6fac0f1
Lluis Santos <lluis@ngynx.com>
2022-02-25 16:37:35 +0100

    first commit, working for now

---
+++ README.md
@@ -0,0 +1,27 @@
+# GrafanaShot
+Phython Tool to get snapshots from Grafana server using selenium.
+
+![PyPI](https://img.shields.io/pypi/v/grafanashot)
+![coverage](https://img.shields.io/badge/coverage-37%25-orange)
+[![Conventional Commits](https://img.shields.io/badge/Conventional%20Commits-1.0.0-yellow.svg)](https://conventionalcommits.org)
+
+## Requirements
+- python 3.7+
+- pip
+
+## Installation
+the recommended way to install GrafanaShot si via **PyPi**.
+
+### Install from [PyPi](https://pypi.org/project/tradingkit/)
+```bash
+pip3 install grafanashot
+```
+
+### Install from [Source](https://github.com/logictraders/tradingkit)
+```bash
+git clone https://github.com/santoslluis/grafanashot
+cd grafanashot
+# Optional, uncomment the line below if you want a specific version
+# git checkout v1.0.0
+python3 setup.py install
+```
---
+++ grafanashot.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+import sys
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support.wait import WebDriverWait
+
+from selenium.webdriver.support import expected_conditions as EC
+
+if len(sys.argv) != 4:
+    print("Usage: %s <dashboard_url> [<user> <pass>]" % sys.argv[0])
+    exit(-1)
+
+full_url = sys.argv[1]
+username = sys.argv[2]
+password = sys.argv[3]
+
+options = Options()
+options.headless = True
+driver = webdriver.Firefox(
+    options=options,
+    executable_path='./bin/geckodriver'
+)
+driver.get(full_url)
+
+# Wait for initialize, in seconds
+wait = WebDriverWait(driver, 30)
+
+username_input = driver.find_element(By.XPATH, '/html/body/div/div/main/div[3]/div/div[2]/div/div/form/div[1]/div[2]/div/div/input')
+
+username_input.click()
+username_input.send_keys(username)
+
+password_input = driver.find_element(By.XPATH, '//*[@id="current-password"]')
+password_input.click()
+password_input.send_keys(password)
+
+login_button = driver.find_element(By.XPATH, '/html/body/div/div/main/div[3]/div/div[2]/div/div/form/button')
+login_button.click()
+
+
+share_button = wait.until(EC.visibility_of_element_located((By.XPATH, '/html/body/div/div/main/div[3]/header/div/div[3]/div/button')))
+share_button.click()
+
+snapshot_tab = driver.find_element(By.XPATH, '/html/body/div[2]/div[2]/div[1]/div[1]/ul/li[2]/a')
+snapshot_tab.click()
+
+snapshot_button = wait.until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[2]/div[2]/div[2]/div/div[5]/div/div[3]/button')))
+snapshot_button.click()
+
+snapshot_link = wait.until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[2]/div[2]/div[2]/div/div[1]/div/a')))
+
+print(snapshot_link.get_attribute('href') + '?kiosk')
