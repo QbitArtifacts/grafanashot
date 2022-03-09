4604dc81455b296a37feaf8a7b8c96b131a984bf
Lluis Santos <lluis@ngynx.com>
2022-03-09 17:51:48 +0100

    added multiurl and timeout 60 seconds

--- grafanashot.py
+++ grafanashot.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import json
 import sys
 
 from selenium import webdriver
@@ -10,22 +11,23 @@ from selenium.webdriver.firefox.service import Service
 
 from selenium.webdriver.support import expected_conditions as EC
 
-if len(sys.argv) != 4:
-    print("Usage: %s <dashboard_url> [<user> <pass>]" % sys.argv[0])
+if len(sys.argv) < 4:
+    print("Usage: %s <user> <pass> (<dashboard_url>)+" % sys.argv[0])
     exit(-1)
 
-full_url = sys.argv[1]
-username = sys.argv[2]
-password = sys.argv[3]
+username = sys.argv[1]
+password = sys.argv[2]
+urls = sys.argv[3:]
 
 options = Options()
 options.headless = True
 service = Service('./bin/geckodriver')
 driver = webdriver.Firefox(options=options, service=service)
-driver.get(full_url)
 
 # Wait for initialize, in seconds
-wait = WebDriverWait(driver, 30)
+wait = WebDriverWait(driver, 60)
+
+driver.get(urls[0])
 
 username_input = driver.find_element(By.XPATH, '/html/body/div/div/main/div[3]/div/div[2]/div/div/form/div[1]/div[2]/div/div/input')
 
@@ -39,30 +41,35 @@ password_input.send_keys(password)
 login_button = driver.find_element(By.XPATH, '/html/body/div/div/main/div[3]/div/div[2]/div/div/form/button')
 login_button.click()
 
+snapshots = {}
+for url in urls:
+    driver.get(url)
 
-share_button = wait.until(EC.visibility_of_element_located((By.XPATH, '/html/body/div/div/main/div[3]/header/div/div[3]/div/button')))
-share_button.click()
+    share_button = wait.until(EC.visibility_of_element_located((By.XPATH, '/html/body/div/div/main/div[3]/header/div/div[3]/div/button')))
+    share_button.click()
 
-snapshot_tab = driver.find_element(By.XPATH, '/html/body/div[2]/div[2]/div[1]/div[1]/ul/li[2]/a')
-snapshot_tab.click()
+    snapshot_tab = driver.find_element(By.XPATH, '/html/body/div[2]/div[2]/div[1]/div[1]/ul/li[2]/a')
+    snapshot_tab.click()
 
-timeout_input = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="timeout-input"]')))
-timeout_input.send_keys('20')  # 20 sec must be far enough to get the data
-timeout_input.send_keys(Keys.ARROW_LEFT)
-timeout_input.send_keys(Keys.ARROW_LEFT)
-timeout_input.send_keys(Keys.BACK_SPACE)
+    timeout_input = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="timeout-input"]')))
+    timeout_input.send_keys('20')  # 20 sec must be far enough to get the data
+    timeout_input.send_keys(Keys.ARROW_LEFT)
+    timeout_input.send_keys(Keys.ARROW_LEFT)
+    timeout_input.send_keys(Keys.BACK_SPACE)
 
-expire_selector = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="expire-select-input"]')))
-expire_selector.click()
-expire_selector.send_keys(Keys.ARROW_DOWN)
-expire_selector.send_keys(Keys.ENTER)
+    expire_selector = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="expire-select-input"]')))
+    expire_selector.click()
+    expire_selector.send_keys(Keys.ARROW_DOWN)
+    expire_selector.send_keys(Keys.ENTER)
 
-snapshot_button = wait.until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[2]/div[2]/div[2]/div/div[5]/div/div[3]/button')))
-snapshot_button.click()
+    snapshot_button = wait.until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[2]/div[2]/div[2]/div/div[5]/div/div[3]/button')))
+    snapshot_button.click()
 
-snapshot_link = wait.until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[2]/div[2]/div[2]/div/div[1]/div/a')))
+    snapshot_link = wait.until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[2]/div[2]/div[2]/div/div[1]/div/a')))
 
-print(snapshot_link.get_attribute('href') + '?kiosk')
+    snapshots[url] = snapshot_link.get_attribute('href')
+
+print(json.dumps(snapshots))
 
 driver.close()
 driver.quit()
