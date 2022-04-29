f6ebde4d08786e77196820b26e5c9354421a38d6
Lluis Santos <lluis@ngynx.com>
2022-04-29 20:31:37 +0200

    bump wait to 120 sec

--- grafanashot.py
+++ grafanashot.py
@@ -53,7 +53,7 @@ for url in urls:
         snapshot_tab.click()
 
         timeout_input = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="timeout-input"]')))
-        timeout_input.send_keys('60')  # 60 sec must be far enough to get the data
+        timeout_input.send_keys('120')  # 60 sec must be far enough to get the data
         timeout_input.send_keys(Keys.ARROW_LEFT)
         timeout_input.send_keys(Keys.ARROW_LEFT)
         timeout_input.send_keys(Keys.BACK_SPACE)
